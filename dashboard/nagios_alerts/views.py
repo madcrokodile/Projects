bdd7b4fd7f8713600a3b4c1d74c3a241c5c25481
anmakarov <mad.crokodile@gmail.com>
2015-02-03 20:40:02 +0300

    a я тупой как бревно

--- dashboard/nagios_alerts/views.py
+++ dashboard/nagios_alerts/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.template import RequestContext, loader, Template
+from django.shortcuts import  render
 
+
+#def index(request):
+#    template = loader.get_template('nagios_alerts/index2.html')
+#    return HttpResponse()
+#    return render_to_response(template)
 
 def index(request):
-    template = loader.get_template('nagios_alerts/index.html')
-    return HttpResponse(template)
-
-
-
+    return render(request, 'nagios_alerts/index.html', {})
