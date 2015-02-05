3df3e75b0ce1781f95a57c3d3e9e7175d6d45ec9
mad.crokodile <mad.crokodile@gmail.com>
2015-02-05 22:54:14 +0300

    я тупой

--- dashboard/nagios_alerts/views.py
+++ dashboard/nagios_alerts/views.py
@@ -1,3 +1,4 @@
+from array import array
 from django.http import HttpResponse
 import datetime
 from django.template import RequestContext, loader, Template
@@ -5,7 +6,10 @@ from django.shortcuts import  render
 
 
 def index(request):
-    dash_template = loader.get_template('nagios_alerts/index.html')
-    context = RequestContext(request,{})
+    server_list = [1, 2, 3]
+    dash_template = loader.get_template('nagios_alerts/index2.html')
+    context = RequestContext(request,
+        {'server_list' : server_list}
+    )
     html = dash_template.render(context)
     return HttpResponse(html)
